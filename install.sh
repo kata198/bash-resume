aa2367c07e60cd684d00e8221b939487997daf08
Tim Savannah <kata198@gmail.com>
2017-06-04 03:36:43 -0400

    install.sh - Do better with DESTDIR and PREFIX

--- install.sh
+++ install.sh
@@ -1,3 +1,45 @@
 #!/bin/bash
 
-install -m 755 bash-resume.sh $DESTDIR/etc
+# Support calling like   ./install.sh DESTDIR="${pkgdir}"
+
+for arg in "$@";
+do
+    if ( echo "${arg}" | grep -q '^PREFIX=' );
+    then
+        export "${arg}"
+    elif ( echo "${arg}" | grep -q '^DESTDIR=' );
+    then
+        export "${arg}"
+    fi
+done
+
+if [ -z "${PREFIX}" ];
+then
+    PREFIX="usr"
+else
+    if [ "${PREFIX}" != "/" ];
+    then
+        PREFIX="$(echo "${PREFIX}" | sed 's|[/][/]*$||g')"
+        PREFIX="$(echo "${PREFIX}" | sed 's|//|/|g')"
+    fi
+fi
+if [ -z "${DESTDIR}" ];
+then
+    DESTDIR=""
+else
+    if [ "${DESTDIR}" != "/" ];
+    then
+        DESTDIR="$(echo "${DESTDIR}" | sed 's|[/][/]*$||g')"
+        DESTDIR="$(echo "${DESTDIR}" | sed 's|//|/|g')"
+    else
+        DESTDIR=''
+    fi
+fi
+
+ETCDIR="${DESTDIR}${PREFIX}/etc"
+# Remove any double-slashes
+ETCDIR="$(echo "${ETCDIR}" | sed 's|//|/|g')"
+
+mkdir -p $DESTDIR/etc
+
+install -v -m 755 bash-resume.sh $DESTDIR/etc/
