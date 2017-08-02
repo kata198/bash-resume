5445134a69111d5d9bdd682dc660719b3dff6d9c
Tim Savannah <kata198@gmail.com>
2017-08-02 00:24:03 -0400

    install.sh:
    * Fix where PREFIX env var or argument was being ignored. DESTDIR worked fine though, so only affected people who wanted /opt installations, for example.
    
    * Add a mode, "INSTALL_DIR" that will override all other logic, ignore
    PREFIX and DESTDIR, and just speciry the directory in which to deploy.
    Use this for custom setups, or scripted install into some site install,
    or in some dot directroy in your hmoe dir, whatever. If you like it
    custom and/or non-global, this is your guy.

--- install.sh
+++ install.sh
@@ -10,36 +10,51 @@ do
     elif ( echo "${arg}" | grep -q '^DESTDIR=' );
     then
         export "${arg}"
+    elif ( echo "${arg}" | grep -q '^INSTALL_DIR=' );
+    then
+        export "${arg}"
     fi
 done
 
-if [ -z "${PREFIX}" ];
+if [ ! -z "${INSTALL_DIR}" ];
 then
-    PREFIX="usr"
-else
-    if [ "${PREFIX}" != "/" ];
+    if [ -z "${PREFIX}" ];
     then
-        PREFIX="$(echo "${PREFIX}" | sed 's|[/][/]*$||g')"
-        PREFIX="$(echo "${PREFIX}" | sed 's|//|/|g')"
-    fi
-fi
-if [ -z "${DESTDIR}" ];
-then
-    DESTDIR=""
-else
-    if [ "${DESTDIR}" != "/" ];
-    then
-        DESTDIR="$(echo "${DESTDIR}" | sed 's|[/][/]*$||g')"
-        DESTDIR="$(echo "${DESTDIR}" | sed 's|//|/|g')"
+        PREFIX="usr"
     else
-        DESTDIR=''
+        if [ "${PREFIX}" != "/" ];
+        then
+            PREFIX="$(echo "${PREFIX}" | sed 's|[/][/]*$||g')"
+            PREFIX="$(echo "${PREFIX}" | sed 's|//|/|g')"
+        fi
     fi
-fi
+    if [ -z "${DESTDIR}" ];
+    then
+        DESTDIR=""
+    else
+        if [ "${DESTDIR}" != "/" ];
+        then
+            DESTDIR="$(echo "${DESTDIR}" | sed 's|[/][/]*$||g')"
+            DESTDIR="$(echo "${DESTDIR}" | sed 's|//|/|g')"
+        else
+            DESTDIR="${DESTDIR}"
+        fi
+    fi
 
-ETCDIR="${DESTDIR}${PREFIX}/etc"
+    export DESTDIR
+    export PREFIX
+    if ! ( ! ( echo "${DESTDIR}" | grep -E '/$' || ! echo "${PREFIX}" | grep -E '^/' ) ); then
+        # Check if combining DESTDIR and PREFIX is not going to naturally have a slash
+        #   and if we need to, nuture it.
+        DESTDIR="${DESTDIR}/"
+    fi
+    FINALDIR="${INSTALL_DIR}"
+else
+    FINALDIR="${DESTDIR}${PREFIX}/etc"
+fi   
 # Remove any double-slashes
-ETCDIR="$(echo "${ETCDIR}" | sed 's|//|/|g')"
+FINALDIR="$(echo "${FINALDIR}" | sed 's|//|/|g')"
 
-mkdir -p $DESTDIR/etc
+mkdir -p ${FINALDIR}
 
-install -v -m 755 bash-resume.sh $DESTDIR/etc/
+install -v -m 755 bash-resume.sh ${FINALDIR}/
